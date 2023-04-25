bb9a501d2d6df2268dbb1a1c1eced1586dc0b945
abim.joshi <abim.joshi@midastechnologies.com.np>
2023-04-25 14:33:46 +0545

    first commit

---
+++ tests/elements.spec.js
@@ -0,0 +1,43 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.beforeEach(async ({ page }) => {
+   await page.goto('https://demoqa.com/elements');
+ });
+test.describe('Validation of  demoqa',  () => {
+   test('Validation of elements ',async ({page}) => { 
+   //  await page.goto('https://demoqa.com/elements')
+
+    await page.locator('span').filter({ hasText: 'Elements' }).locator('svg').nth(1).click()
+    await expect(await page.getByRole('listitem').filter({ hasText: 'Text Box' })).toBeVisible();
+
+
+    await page.getByRole('listitem').filter({ hasText: 'Text Box' }).click()
+    await expect(await page.locator('span').filter({ hasText: 'Elements' }).locator('div').nth(2)).toBeVisible();
+
+    const fullname  = "Nikunj Basnett"
+    await page.getByPlaceholder('Full Name').type(fullname)
+    await expect(await page.getByPlaceholder('Full Name')).toBeVisible();
+
+    const email = "test123@test.com"
+    await page.getByPlaceholder('name@example.com').type(email)
+    await expect(await page.getByPlaceholder('name@example.com')).toBeVisible();
+
+    const address = "Gharipatan"
+    await page.getByPlaceholder('Current Address').type(address)
+    await expect(await page.getByPlaceholder('Current Address')).toBeVisible();
+
+    const permanentaddress ="pokhara"
+    await page.locator('#permanentAddress').type(permanentaddress)
+    await expect(await page.locator('#permanentAddress')).toBeVisible();
+
+    await page.getByRole('button', { name: 'Submit' }).click()
+
+    await page.getByText('Name:Nikunj BasnetEmail:test123@test.comCurrent Address :Gharipatan Permananet A')
+
+    await expect(await page.getByText('Name:Nikunj BasnetEmail:test123@test.comCurrent Address :Gharipatan Permananet A')).toBeVisible();
+
+   }) 
+
+   
+})
---
+++ tests/forms.spec.js
@@ -0,0 +1,55 @@
+ 
+ const { test, expect } = require('@playwright/test');
+ 
+ test.beforeEach(async ({ page }) => {
+  await page.goto('https://demoqa.com/automation-practice-form');
+ });
+ 
+ 
+ test.describe('validation of form ', () => {
+    test('Validation of student registration form ', async ({ page }) => {
+        await page.getByRole('link')
+        await expect(await page.getByRole('link')).toBeVisible();
+
+        await page.getByPlaceholder('First Name').type('Nikunj');
+        await expect(await page.getByPlaceholder('First Name')).toBeVisible();
+
+        await page.getByPlaceholder('Last Name').type('Basnet');
+        await expect(await page.getByPlaceholder('Last Name')).toBeVisible();
+        
+        await page.getByPlaceholder('name@example.com').type('abc@test.com')
+        await expect(await page.getByPlaceholder('Last Name')).toBeVisible();
+
+        await page.getByText('Male', { exact: true }).check()
+        await expect(await page.getByText('Male', { exact: true })).toBeChecked();
+
+        await page.getByPlaceholder('Mobile Number').type('9816615595')
+        await expect(await page.getByPlaceholder('Mobile Number')).toBeVisible();
+
+
+        await page.locator('#dateOfBirthInput').click()
+        await page.locator('div').filter({ hasText: /^JanuaryFebruaryMarchAprilMayJuneJulyAugustSeptemberOctoberNovemberDecember$/ }).getByRole('combobox').selectOption('May')
+        
+        await page.getByRole('option', { name: 'Choose Thursday, May 4th, 2023' }, { timeout: 5000, visible: true }).click();
+         
+        await page.getByPlaceholder('Current Address').type('Kaski')
+        await page.locator('#state svg')
+        await page.getByText('NCR', { exact: true })
+        // await expect(await page.getByText('NCR', { exact: true })).toBeVisible();
+        await page.locator('div').filter({ hasText: /^Select State$/ }).nth(3).selectOption('NCR')
+        
+
+        
+    })
+
+    
+
+
+    
+ });
+
+ 
+
+ 
+
+ 
